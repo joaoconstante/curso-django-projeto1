5bba95f181b39cdc06830b3b4c49bc3cbc644e9c
João Vitor Constante <joaocon99@gmail.com>
2023-06-06 16:48:25 -0300

    Changes to be committed: 	new file:   __init__.py 	new file:   asgi.py 	modified:   projeto/settings.py 	modified:   projeto/urls.py 	new file:   recipes/__init__.py 	new file:   recipes/admin.py 	new file:   recipes/apps.py 	new file:   recipes/migrations/__init__.py 	new file:   recipes/models.py 	new file:   recipes/templates/recipes/home.html 	new file:   recipes/tests.py 	new file:   recipes/urls.py 	new file:   recipes/views.py 	new file:   settings.py 	new file:   urls.py 	new file:   wsgi.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import path
+from recipes.views import home, contato, sobre
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
-    path('recipes/', include('recipes.urls')),
+    path('', home),
 ]
